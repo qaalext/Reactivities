3cadb0d8d65b91f9f9b6bf6189f9639edca3162f
Alexandru Rendec <qaalexr@gmail.com>
2021-09-19 13:45:29 +0300

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -32,7 +33,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.Users.Include(p => p.Photos)
+            .FirstOrDefaultAsync(x => x.Email == loginDto.Email);
+
             if (user == null)
             {
                 return Unauthorized();
@@ -76,7 +79,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(p => p.Photos).
+            FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
             return CreateUserObject(user);
 
         }
@@ -85,7 +89,7 @@ namespace API.Controllers
              return new UserDto
                 {
                     DisplayName = user.DisplayName,
-                    Image = null,
+                    Image = user?.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
                     Token = _tokenService.CreateToken(user),
                     Username = user.UserName
                 };
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,8 @@ namespace Persistence
         }
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; } // < getting table names from here
+        
         
         protected override void OnModelCreating(ModelBuilder builder){
             
