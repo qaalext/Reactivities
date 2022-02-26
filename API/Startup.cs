015b98ea19328763e831b0cbaf5baf76f7eb8184
Alexandru Rendec <qaalexr@gmail.com>
2022-02-26 20:31:52 +0200

    Heroku deploy

--- API/Startup.cs
+++ API/Startup.cs
@@ -70,6 +70,10 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseCors("CorsPolicy");
             
             app.UseAuthentication();
@@ -79,6 +83,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chat");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -11,7 +11,7 @@ const sleep = (delay: number) => {
     return new Promise((resolve) => { setTimeout(resolve, delay) })
 }
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.request.use(config => {
     const token = store.commonStore.token;
@@ -21,7 +21,8 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
 
-        await sleep(1000);
+    if(process.env.NODE_ENV === 'development') await sleep(1000);
+        
         const pagination = response.headers['pagination'];
         if(pagination){
             response.data = new PaginatedResult(response.data, JSON.parse(pagination));
